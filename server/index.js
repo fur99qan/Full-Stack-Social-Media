a9490f75a5cd7667ab866646ae2b1d5a7788680a
Muhammad Furqan <87532356+fur99qan@users.noreply.github.com>
2022-08-21 21:48:57 +0500

    final commit

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ app.use(express.json());
 app.use(cors());
 
 const db = require("./models");
+const port = process.env.PORT || 3001
 
 // Routers
 const postRouter = require("./routes/Posts");
@@ -17,8 +18,14 @@ app.use('/auth', usersRouter)
 const likesRouter = require("./routes/Likes");
 app.use('/likes', likesRouter)
 
-db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
-        console.log("Server running on port 3001");
+db.sequelize
+    .sync()
+    .then(() => {
+        app.listen(port, () => {
+            console.log("Server running on port 3001");
+        });
+
+    })
+    .catch((error) => {
+        console.log(error);
     });
-});
